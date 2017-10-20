bcdb748a6d677f99c2390518784f66079ef5185f
Etsuji Nakai <enakai0@gmail.com>
2017-10-20 22:01:30 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,47 @@
+# tensorflow-sketch-rnn-example
+
+## Quick Install
+
+Launch GCE instance.
+- 2vCPU (7.5G Memory)
+- Debian GNU/Linux 9 (stretch)
+- Allow HTTP traffic
+
+Install prereq packages.
+```
+sudo -i
+apt-get update
+apt-get install -y build-essential python-pip unzip python-cairosvg git
+pip install ipython==5.5.0 rdp==0.8 svgwrite==1.1.6 tensorflow==1.3.0 Flask==0.12.2
+pip install magenta 
+```
+
+Download pre-trained models.
+```
+mkdir -p /opt/sketch_demo/models
+cd /opt/sketch_demo/models
+curl -OL http://download.magenta.tensorflow.org/models/sketch_rnn.zip
+unzip sketch_rnn.zip
+```
+
+Install demo app.
+```
+cd $HOME
+git clone https://github.com/enakai00/tensorflow-sketch-rnn-example
+cp -a tensorflow-sketch-rnn-example/sketch_demo /opt/
+cp /opt/sketch_demo/sketch_demo.service /etc/systemd/system/
+```
+
+This application provides a simple user authentication mechanism. You can change the username and password by modifying the following part in /opt/sketch_demo/auth_decorator.py.
+```
+USERNAME = 'username'
+PASSWORD = 'passw0rd'
+```
+
+Launch the demo application.
+```
+systemctl daemon-reload
+systemctl enable sketch_demo
+systemctl start sketch_demo
+systemctl status sketch_demo
+```
---
+++ sketch_demo/app.py
@@ -0,0 +1,172 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Copyright 2017 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import base64
+import cStringIO
+import tempfile
+
+from auth_decorator import requires_auth
+from cairosvg import svg2png
+from flask import abort
+from flask import Flask
+from flask import jsonify
+from flask import make_response
+from flask import redirect
+from flask import render_template
+from flask import request
+from flask import url_for
+import numpy as np
+from PIL import Image
+from rdp import rdp
+import svgwrite
+import tensorflow as tf
+
+from magenta.models.sketch_rnn.sketch_rnn_train import *
+from magenta.models.sketch_rnn.model import *
+from magenta.models.sketch_rnn.utils import *
+from magenta.models.sketch_rnn.rnn import *
+
+
+app = Flask(__name__)
+
+
+@app.before_request
+@requires_auth
+def before_request():
+  pass
+
+MODEL_DIR = '/opt/sketch_demo/models/catbus/lstm'
+
+def draw_strokes(data, factor=0.12):
+  with tempfile.NamedTemporaryFile() as temp_svg:
+    min_x, max_x, min_y, max_y = get_bounds(data, factor)
+    dims = (50 + max_x - min_x, 50 + max_y - min_y)
+    dwg = svgwrite.Drawing(temp_svg.name, size=dims)
+    dwg.add(dwg.rect(insert=(0, 0), size=dims,fill='white'))
+    lift_pen = 1
+    abs_x, abs_y = 25 - min_x, 25 - min_y
+    p = "M%s,%s " % (abs_x, abs_y)
+    command = "m"
+    for i in xrange(len(data)):
+      if (lift_pen == 1):
+        command = "m"
+      elif (command != "l"):
+        command = "l"
+      else:
+        command = ""
+      x, y = float(data[i,0])/factor, float(data[i,1])/factor
+      lift_pen = data[i, 2]
+      p += command+str(x)+","+str(y)+" "
+    the_color = "black"
+    stroke_width = 1
+    dwg.add(dwg.path(p).stroke(the_color,stroke_width).fill("none"))
+    dwg.save()
+    temp_svg.flush()
+     
+    with tempfile.NamedTemporaryFile() as temp_png:
+      svg2png(url=temp_svg.name, write_to=temp_png.name)
+      image = Image.open(temp_png.name)
+      image_buffer = cStringIO.StringIO()
+      image.save(image_buffer, format='PNG')
+      imgstr = 'data:image/png;base64,{:s}'.format(
+          base64.b64encode(image_buffer.getvalue()))
+  return imgstr
+
+
+class Sketch_generator(object):
+
+  def __init__(self):
+    [hps_model, eval_hps_model, sample_hps_model] = load_model(MODEL_DIR)
+    model = Model(hps_model)
+    eval_model = Model(eval_hps_model, reuse=True)
+    sample_model = Model(sample_hps_model, reuse=True)
+    sess = tf.InteractiveSession()
+    sess.run(tf.global_variables_initializer())
+    load_checkpoint(sess, MODEL_DIR)
+
+    self.sess = sess
+    self.eval_model = eval_model
+    self.sample_model = sample_model
+
+
+  def encode(self, image):
+
+    def _compress_stroke(strokes):
+      result = [[x/15.0, y/15.0, 0] for [x,y] in rdp(strokes, epsilon=1.0)]
+      result[-1][2] = 1
+      return result
+
+    result = []
+    strokes = []
+    for i in range(0, len(image), 3):
+        x, y, pen_down = image[i], image[i+1], image[i+2]
+        if pen_down == 0:
+            if len(strokes) > 0:
+                result += _compress_stroke(strokes)
+                strokes = [[x,y]]
+        else:
+            strokes.append([x,y])
+    if len(strokes) > 0:
+        result += _compress_stroke(strokes)
+    
+    pre_x, pre_y = 0, 0
+    for i, (x, y, pen) in enumerate(result):
+        result[i] = [x - pre_x, y - pre_y, pen]
+        pre_x, pre_y = x, y
+
+    strokes = to_big_strokes(np.array(result)).tolist()
+    strokes = strokes[:151]
+    seq_len = [min(150, len(result))]
+    z = self.sess.run(self.eval_model.batch_z, 
+                      feed_dict={self.eval_model.input_data: [strokes],
+                                 self.eval_model.sequence_lengths: seq_len})[0]
+    return draw_strokes(to_normal_strokes(np.array(strokes))), z
+
+
+  def decode(self, z_input, temperature=0.1):
+    z = [z_input]
+    sample_strokes, m = sample(self.sess, self.sample_model,
+                               seq_len=self.eval_model.hps.max_seq_len,
+                               temperature=temperature, z=z)
+    strokes = to_normal_strokes(sample_strokes)
+    return draw_strokes(strokes)
+
+
+@app.route('/')
+def canvas():
+  return render_template('canvas.html')
+
+
+@app.route('/post', methods=['GET', 'POST'])
+def post():
+  strokes = request.get_json()
+  if len(strokes) == 0:
+    abort(make_response(jsonify(message="no sample"), 400))
+  original, z = client.encode(strokes)
+  kid1 = client.decode(z, temperature=0.1)
+  kid2 = client.decode(z, temperature=0.5)
+  kid3 = client.decode(z, temperature=0.9)
+  result = json.dumps({'kid1': kid1, 'kid2': kid2, 'kid3': kid3,
+                       'original': original})
+  return result
+
+
+client = Sketch_generator()
+
+
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', port=80, debug=True)
